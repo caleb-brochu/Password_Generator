36b2c70c30e68f33c0884a973a645d0f2dc9e966
Caleb Brochu <caleb.brochu@gmail.com>
2019-12-17 22:04:49 -0800

    Completed random generator in script.html

--- script.js
+++ script.js
@@ -22,37 +22,103 @@ document.getElementById("generate").addEventListener("click", function userPromp
 
 function passwordGenerator(upper, lower, number, special, length)
 {
+    
     console.log(upper);
     console.log(lower);
     console.log(number);
     console.log(special);
     console.log(length);
-
-    let choices = upper + lower + special + number; 
+   
     let password = "";
-    console.log("password", password); 
-    for(let i = 0; i < length; i++){
-        if(upper && password.length < length)
-        {
-            password += getUpper();  
-        }
-        if(lower && password.length < length)
-        {
-            password += getLower();
-        }
-        if(number && password.length < length)
-        {
-            password += getNumber();
-        }
-        if(special && password.length < length)
-        {
-            password += getSpecial();
+    let passwordLength = password.length;
+    // let myarray = [charUpper,charLower,charNumber,charSpecial];
+    // for(let i = 0; i < length; i++){
+    
+        
+
+    // }
+    let randomChar = {
+        charUpper: function()
+        { 
+            if(upper && password.length < length)
+            {
+                password += getUpper();  
+            }
+            return password;
+        }, 
+        charLower: function()
+        {        
+            if(lower && password.length < length)
+            {
+                password += getLower();
+            }
+            return password;
+        },
+        charNumber: function()
+        {        
+            if(number && password.length < length)
+            {
+                password += getNumber();
+            }
+            return password;
+        },
+        charSpecial: function()
+        {        
+            if(special && password.length < length)
+            {
+                password += getSpecial();
+            }
+            return password;
         }
     }
-    console.log("password", password);
-    return password;
-}
 
+
+    while(password.length < length){
+        
+        const keys = Object.keys(randomChar);
+        const randomNum = keys[Math.floor(Math.random() * keys.length)];
+        switch(randomNum){
+            
+            case 'charUpper':
+                randomChar.charUpper();
+                break;
+            case 'charLower':
+                randomChar.charLower();
+                break;
+            case 'charNumber':
+                randomChar.charNumber();
+                break;
+            case 'charSpecial':
+                randomChar.charSpecial();
+                break;
+        }
+    }
+    
+
+//     let password = "";
+//     console.log("password", password); 
+//     
+//         if(upper && password.length < length)
+//         {
+//             password += getUpper();  
+//         }
+//         if(lower && password.length < length)
+//         {
+//             password += getLower();
+//         }
+//         if(number && password.length < length)
+//         {
+//             password += getNumber();
+//         }
+//         if(special && password.length < length)
+//         {
+//             password += getSpecial();
+//         }
+//     }
+//     console.log("password", password);
+     console.log(password);
+
+}
 function getUpper()
 {
     return String.fromCharCode(Math.floor(Math.random()* 26) + 65);
